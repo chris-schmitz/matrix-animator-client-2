a27f0721e948fdcd8b44c307c08bd074cdc5f8ce
Christopher Schmitz <schmitz.chris@gmail.com>
2024-01-25 21:58:52 -0600

    Abstracting duplicative code for button rendering

--- src/components/WorkArea.js
+++ src/components/WorkArea.js
@@ -1,15 +1,15 @@
-import {useState} from "react";
-import Grid from "./Grid";
-import {addButtonPressedClass, removeButtonPressedClass} from "../utilities/mouseUtilities";
+import { useState } from "react"
+import Grid from "./Grid"
+import { addButtonPressedClass, removeButtonPressedClass } from "../utilities/mouseUtilities"
 
 export default function WorkArea({
-                                     animationFrame,
-                                     handleAnimationFrameUpdate,
-                                     handleNewFrameRequest,
-                                     handleDeleteFrameRequest,
-                                     handleDuplicateFrameRequest
-                                 }) {
-    const [pickers, setPickers] = useState({activeIndex: 0, color: ['#FF00FF', '#00ff00', '#FFFFFF']})
+    animationFrame,
+    handleAnimationFrameUpdate,
+    handleNewFrameRequest,
+    handleDeleteFrameRequest,
+    handleDuplicateFrameRequest
+}) {
+    const [pickers, setPickers] = useState({ activeIndex: 0, color: ['#FF00FF', '#00ff00', '#FFFFFF'] })
     const [paintPixels, setPaintPixels] = useState(false)
 
     function handlePixelClick(rowIndex, columnIndex) {
@@ -19,13 +19,13 @@ export default function WorkArea({
     }
 
     function storePickerColor(color, pickerIndex) {
-        const pickersUpdate = {...pickers}
+        const pickersUpdate = { ...pickers }
         pickersUpdate.color[pickerIndex] = color
         setPickers(pickersUpdate)
     }
 
     function handleSetActiveColor(pickerIndex) {
-        const pickersUpdate = {...pickers}
+        const pickersUpdate = { ...pickers }
         pickersUpdate.activeIndex = pickerIndex
         setPickers(pickersUpdate)
     }
@@ -34,44 +34,67 @@ export default function WorkArea({
         setPaintPixels(canPaint)
     }
 
-    return (
-        <div data-testid="workarea" className="work-area">
-            <div className="frame-actions">
+    function renderPaletteButtonGroups() {
+        // * hmmmmm what if we make the number of color pickers configureable ... 
+        const totalPaletteButtons = 3
+        const paletteButtonGroups = []
+        for (let i = 0; i < totalPaletteButtons; i++)
+        {
+            paletteButtonGroups.push(
+                <div className="palette-button-group">
+                    <input
+                        data-testid="color-picker"
+                        type="color"
+                        value={pickers.color[i]}
+                        onChange={(event) => {
+                            storePickerColor(event.target.value, i)
+                        }}
+                    />
+                    <input
+                        name="pickerActivator"
+                        className="picker-activator"
+                        type="radio"
+                        data-testid="color-picker-activator"
+                        checked={pickers.activeIndex === i}
+                        onChange={() => handleSetActiveColor(i)}
+                    />
+                </div>
+            )
+        }
+        return paletteButtonGroups
+    }
+
+    function renderFrameActionButtons() {
+        const buttonData = [
+            { name: "new-frame-button", clickHandler: handleNewFrameRequest, symbol: "+" },
+            { name: "duplicate-frame-button", clickHandler: () => handleDuplicateFrameRequest(animationFrame.id), symbol: "++" },
+            { name: "delete-frame-button", clickHandler: () => handleDeleteFrameRequest(animationFrame.id), symbol: "-" },
+        ]
+        const buttons = []
+        for (let i = 0; i < 3; i++)
+        {
+            buttons.push(
                 <button
-                    data-testid="new-frame-button"
-                    onClick={handleNewFrameRequest}
-                    onMouseDown={(event) => event.target.classList.add("pressed")}
-                    onMouseUp={(event) => event.target.classList.remove("pressed")}
-                    onMouseLeave={(event) => event.target.classList.remove("pressed")}
-                    className="new-frame-button"
-                >
-                    +
-                </button>
-                <button
-                    data-testid="duplicate-frame-button"
-                    onClick={() => handleDuplicateFrameRequest(animationFrame.id)}
-                    className="duplicate-frame-button"
-                    onMouseDown={(event) => event.target.classList.add("pressed")}
-                    onMouseUp={(event) => event.target.classList.remove("pressed")}
-                    onMouseLeave={(event) => event.target.classList.remove("pressed")}
-                >
-                    ++
-                </button>
-                <button
-                    data-testid="delete-frame-button"
-                    onClick={() => handleDeleteFrameRequest(animationFrame.id)}
-                    className="delete-frame-button"
+                    data-testid={buttonData[i].name}
+                    className={buttonData[i].name}
+                    onClick={buttonData[i].clickHandler}
                     onMouseDown={addButtonPressedClass}
                     onMouseUp={removeButtonPressedClass}
                     onMouseLeave={removeButtonPressedClass}
                 >
-                    -
+                    {buttonData[i].symbol}
                 </button>
+            )
+        }
+        return buttons
+    }
+
+    return (
+        <div data-testid="workarea" className="work-area">
+            <div className="frame-actions">
+                {renderFrameActionButtons()}
             </div>
-            <div
-                className="grid-wrapper"
-                onMouseLeave={() => handleSetPaintPixels(false)}
-            >
+            <div className="grid-wrapper" onMouseLeave={() => handleSetPaintPixels(false)} >
                 <Grid
                     height={animationFrame.height}
                     width={animationFrame.width}
@@ -82,62 +105,8 @@ export default function WorkArea({
                 />
             </div>
             <div className="color-palette" data-testid="palette">
-                <div className="palette-button-group">
-                    <input
-                        data-testid="color-picker"
-                        type="color"
-                        value={pickers.color[0]}
-                        onChange={(event) => {
-                            storePickerColor(event.target.value, 0)
-                        }}
-                    />
-                    <input
-                        name="pickerActivator"
-                        className="picker-activator"
-                        type="radio"
-                        data-testid="color-picker-activator"
-                        checked={pickers.activeIndex === 0}
-                        onChange={() => handleSetActiveColor(0)}
-                    />
-                </div>
-                <div className="palette-button-group">
-                    <input
-                        data-testid="color-picker"
-                        type="color"
-                        value={pickers.color[1]}
-                        onChange={(event) => {
-                            storePickerColor(event.target.value, 1)
-                        }}
-                    />
-                    <input
-                        name="pickerActivator"
-                        className="picker-activator"
-                        type="radio"
-                        data-testid="color-picker-activator"
-                        checked={pickers.activeIndex === 1}
-                        onChange={() => handleSetActiveColor(1)}
-                    />
-                </div>
-                <div className="palette-button-group">
-                    <input
-                        data-testid="color-picker"
-                        type="color"
-                        value={pickers.color[2]}
-                        onChange={(event) => {
-                            storePickerColor(event.target.value, 2)
-                        }}
-                    />
-                    <input
-                        name="pickerActivator"
-                        className="picker-activator"
-                        type="radio"
-                        data-testid="color-picker-activator"
-                        checked={pickers.activeIndex === 2}
-                        onChange={() => handleSetActiveColor(2)}
-                    />
-                </div>
+                {renderPaletteButtonGroups()}
             </div>
-
         </div>
-    );
+    )
 }
