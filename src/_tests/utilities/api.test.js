7fd7434bf3f8e2c20475a3e4e64ae20955fa81f1
Christopher Schmitz <cschmitz@labelinsight.com>
2024-01-31 20:50:26 -0600

    WIP: starts getAnimation test

--- src/_tests/utilities/api.test.js
+++ src/_tests/utilities/api.test.js
@@ -1,42 +1,61 @@
-import { saveAnimation } from "../../utilities/apis"
-import { AnimationFrame, AnimationRequestPayload } from "../../domain/AnimationFrame"
+import {getAnimation, saveAnimation} from "../../utilities/apis"
+import {AnimationFrame, AnimationRequestPayload} from "../../domain/AnimationFrame"
 
 function mockFetchSuccessfulResponse(content) {
-  global.fetch = jest.fn(() => {
-    return Promise.resolve({
-      json: () => Promise.resolve(content),
-      text: () => Promise.resolve(content)
+    global.fetch = jest.fn(() => {
+        return Promise.resolve({
+            json: () => Promise.resolve(content),
+            text: () => Promise.resolve(content)
+        })
     })
-  })
 }
 
 describe("animations api", () => {
-  it("can save an animation", async () => {
-    const expectedId = 123
-    mockFetchSuccessfulResponse(expectedId)
-    const frames = [
-      new AnimationFrame(0, 8, 8, [1])
-    ]
-    const animation = new AnimationRequestPayload(
-      "test",
-      1,
-      frames[0].height,
-      frames[0].width,
-      frames[0].speed,
-      frames
-    )
+    it("can save an animation", async () => {
+        const expectedId = 123
+        mockFetchSuccessfulResponse(expectedId)
+        const frames = [
+            new AnimationFrame(0, 8, 8, [1])
+        ]
+        const animation = new AnimationRequestPayload(
+            "test",
+            1,
+            frames[0].height,
+            frames[0].width,
+            frames[0].speed,
+            frames
+        )
 
-    const actualId = await saveAnimation(animation)
+        const actualId = await saveAnimation(animation)
 
-    expect(fetch).toHaveBeenCalledWith(
-      "http://localhost:8080/rest/animations",
-      {
-        "method": "POST",
-        headers: {
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify(animation)
-      })
-    expect(actualId).toBe(expectedId)
-  })
+        expect(fetch).toHaveBeenCalledWith(
+            "http://localhost:8080/rest/animations",
+            {
+                "method": "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(animation)
+            })
+        expect(actualId).toBe(expectedId)
+    })
+
+    it("can get an animation by ID", async () => {
+        const animationId = 123
+        const expected = new AnimationRequestPayload("test animation", 3, 1, 1, 1, [new AnimationFrame(1, 1, 1, [0xFFFFFF])], animationId)
+        mockFetchSuccessfulResponse()
+
+        const actual = getAnimation(animationId)
+
+        expect(fetch).toHaveBeenCalledWith(
+            `http://localhost:8080/rest/animations/${animationId}`,
+            {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            }
+        )
+        expect(actual).toBe(expected)
+    })
 })
--- src/utilities/apis.js
+++ src/utilities/apis.js
@@ -1,5 +1,14 @@
 export async function getAnimation(id) {
-
+    const response = await fetch(
+        `http://localhost:8080/rest/animations/${id}`,
+        {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json"
+            },
+        }
+    )
+    const content = await resp
 }
 
 export async function getAnimationList() {
@@ -7,13 +16,13 @@ export async function getAnimationList() {
 }
 
 export async function saveAnimation(animationRequestPayload) {
-  const response = await fetch("http://localhost:8080/rest/animations", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify(animationRequestPayload)
-  })
-  const content = await response.text()
-  return content
+    const response = await fetch("http://localhost:8080/rest/animations", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(animationRequestPayload)
+    })
+    const content = await response.text()
+    return content
 }
